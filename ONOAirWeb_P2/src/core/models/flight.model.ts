a60920a3aa51c82f12016105bb10d45e1a4f856b
OmriKadouri97 <144044822+OmriKadouri97@users.noreply.github.com>
2025-01-25 22:53:41 +0200

    Writing the code for flight model

---
+++ ONOAirWeb_P2/src/core/models/flight.model.ts
@@ -0,0 +1,9 @@
+export interface Flight {
+    flightNumber: string; // Unique flight identifier
+    departureCode: string; // Departure destination code
+    arrivalCode: string; // Landing destination code
+    departureDateTime: string; // Departure date and time (ISO format)
+    arrivalDateTime: string; // Landing date and time (ISO format)
+    numberOfSeats: number; // Total number of seats on the flight
+  }
+  
