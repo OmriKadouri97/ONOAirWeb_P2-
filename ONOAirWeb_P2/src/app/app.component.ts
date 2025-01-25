6192beb772d4da070fe5f046a94a75f556cc1c85
OmriKadouri97 <144044822+OmriKadouri97@users.noreply.github.com>
2025-01-25 22:23:11 +0200

    Writing Code For app.component

--- ONOAirWeb_P2/src/app/app.component.ts
+++ ONOAirWeb_P2/src/app/app.component.ts
@@ -1,11 +1,18 @@
+import { FooterComponent } from '../shared/footer/footer.component';
+import { HeaderComponent } from '../shared/header/header.component';
+import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone:true,
+  imports: [FooterComponent, HeaderComponent, RouterModule], // Ensure paths are correct
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+    <app-footer></app-footer>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ONOAirWeb_P2';
